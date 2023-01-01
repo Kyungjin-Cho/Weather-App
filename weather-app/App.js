9ae3348c68e6a8d239676905381c4ab2a5e9da6a
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-01-01 21:58:57 +0000

    Update App.js

--- weather-app/App.js
+++ weather-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.text}>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'beige',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 48,
+    color: 'teal', 
+  }
 });
