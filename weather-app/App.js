90e19faa130412c69dd73a3c88bae894a305ab6e
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-01-03 14:45:53 +0000

    Update App.js

--- weather-app/App.js
+++ weather-app/App.js
@@ -43,7 +43,7 @@ export default function App() {
     );
     setCity(location[0].city);
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
     );
     const json = await response.json();
     setDays(json.daily);
